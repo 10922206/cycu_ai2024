f47010fb93ed843a54e5fcfe185f77e88018eb8b
李昀澄 <160597467+10922206@users.noreply.github.com>
2024-02-27 22:34:37 +0800

    test 2

--- 20240227/10922206.py
+++ 20240227/10922206.py
@@ -1,7 +1,12 @@
 print("Hello, World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
+import csv
 import requests
 from bs4 import BeautifulSoup
+import os
+
+# 打印當前工作目錄
+print("當前工作目錄:", os.getcwd())
 
 # 發送 GET 請求
 response = requests.get('https://news.pts.org.tw/xml/newsfeed.xml')
@@ -12,12 +17,15 @@ soup = BeautifulSoup(response.content, 'xml')
 # 找到所有的 <title> 標籤
 titles = soup.find_all('title')
 
-# 打印出所有的標題
-for title in titles:
-    print(title.text)
-    #印出 summary
-    print(title.find_next('summary').text)
-    print()
-    #印出分隔線
-    print("=====================================")
-    print()
+# 打開 CSV 檔案進行寫入
+with open('news_titles_and_summaries.csv', mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['標題', '摘要'])  # 寫入 CSV 檔案的標題行
+
+    # 將標題和摘要寫入 CSV 檔案
+    for title in titles:
+        title_text = title.text.strip()
+        summary_text = title.find_next('summary').text.strip()
+        writer.writerow([title_text, summary_text])
+
+print("CSV 檔案已成功寫入到指定路徑")
