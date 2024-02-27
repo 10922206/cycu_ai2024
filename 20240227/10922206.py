2d0cb10a9dd174921246a4e368bb4f66e142ba30
李昀澄 <160597467+10922206@users.noreply.github.com>
2024-02-27 20:18:37 +0800

    test

--- 20240227/10922206.py
+++ 20240227/10922206.py
@@ -1,1 +1,23 @@
-print("Hello, World!")
+print("Hello, World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+import requests
+from bs4 import BeautifulSoup
+
+# 發送 GET 請求
+response = requests.get('https://news.pts.org.tw/xml/newsfeed.xml')
+
+# 解析 XML 數據
+soup = BeautifulSoup(response.content, 'xml')
+
+# 找到所有的 <title> 標籤
+titles = soup.find_all('title')
+
+# 打印出所有的標題
+for title in titles:
+    print(title.text)
+    #印出 summary
+    print(title.find_next('summary').text)
+    print()
+    #印出分隔線
+    print("=====================================")
+    print()
