53241299485cae5b7bf2d391c338bbdcbe9055c2
李昀澄 <160597467+10922206@users.noreply.github.com>
2024-04-23 20:47:37 +0800

    re

---
+++ 20240423/20240423.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib.request
+import os
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+def download_csv_files(url, download_path):
+    response = requests.get(url, verify=False)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href and href.endswith('.csv'):
+            file_url = urllib.parse.urljoin(url, href)
+            file_name = os.path.join(download_path, os.path.basename(href))
+            urllib.request.urlretrieve(file_url, file_name)
+
+# 使用方式
+base_url = 'https://tisvcloud.freeway.gov.tw/history/TDCS/M03A/20240421/'
+download_path = 'C:\\Users\\User\\Downloads\\M03A'
+
+for i in range(1, 25):
+    url = base_url + str(i).zfill(2) + '/'
+    download_csv_files(url, download_path)
